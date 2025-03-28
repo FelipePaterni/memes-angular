68f7d1589a7f18851d41d2242ca94449f211480d
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-03-28 15:06:06 -0300

    arquivos iniciais

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { MemesListComponent } from './memes-list/memes-list.component';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MemesListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/memes-list/memes-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MemeService } from '../meme.service';
+
+@Component({
+  selector: 'app-memes-list',
+  standalone: false,
+  templateUrl: './memes-list.component.html',
+  styleUrl: './memes-list.component.css',
+})
+export class MemesListComponent implements OnInit {
+  memes: any[] = [];
+  constructor(private service: MemeService) {}
+
+  ngOnInit(): void {
+    this.service.getMemes().subscribe((json) => (this.memes = json.data.memes));
+  }
+}
